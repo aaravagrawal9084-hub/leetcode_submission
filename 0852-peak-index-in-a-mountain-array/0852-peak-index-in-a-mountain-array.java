8af89fe43953713e3d7505edd20dbec4fb7d9c20
aaravagrawal9084-hub <aaravagrawal9084@gmail.com>
2026-08-28 02:27:46 +0530

    Time: 0 ms (100%), Space: 80 MB (94.94%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
@@ -7,7 +7,7 @@ class Solution {
             if(arr[mid]>arr[mid+1] && arr[mid]>arr[mid-1]){
                 return mid;
             }
-            else if(arr[mid]>arr[mid+1] && arr[mid]<arr[mid-1]){
+            else if(arr[mid]>arr[mid+1]){
                 hi = mid-1;
             }
             else{
