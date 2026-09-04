5645c7ab0d35029803a0cf75017ac88d043fbd85
aaravagrawal9084-hub <aaravagrawal9084@gmail.com>
2026-09-05 02:57:08 +0530

    Time: 195 ms (19.85%), Space: 51.3 MB (8.02%) - LeetHub

---
+++ 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] nextLargerNodes(ListNode head) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        ListNode temp = head;
+        while(temp!=null){
+            ans.add(temp.val);
+            temp = temp.next;
+        }
+        int[] arr = new int[ans.size()];
+        Stack<Integer> st = new Stack<>();
+        st.push(0);
+        for(int i = 1;i<ans.size();i++){
+            if(st.size()!=0 && ans.get(st.peek())>ans.get(i)){
+                st.push(i);
+            }
+            else{
+                while(st.size()!=0 && ans.get(st.peek())<ans.get(i)){
+                    arr[st.peek()] = ans.get(i);
+                    System.out.print(arr[st.peek()]+" ");
+                    st.pop();
+                }
+                st.push(i);
+            }
+        }
+        return arr;
+
+    }
+}
