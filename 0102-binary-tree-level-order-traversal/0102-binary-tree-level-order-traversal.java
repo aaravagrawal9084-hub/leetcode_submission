e5b77a4249271807346ea6c6da04798c8ccd5307
aaravagrawal9084-hub <aaravagrawal9084@gmail.com>
2026-09-19 16:13:28 +0530

    Time: 2 ms (19%), Space: 46.6 MB (80.04%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -1,3 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
 class Pair{
     TreeNode node;
     int level;
@@ -10,25 +26,27 @@ class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
         ArrayList<List<Integer>> ans = new ArrayList<>();
         if(root==null) return ans;
-        ArrayList<Integer> List = new ArrayList<>();
+        bfs(root,ans);
+        return ans;
+    }
+    public void bfs(TreeNode root, ArrayList<List<Integer>> ans){
         Queue<Pair> q = new LinkedList<>();
-        int prevlevel = 0;
+        ArrayList<Integer> list = new ArrayList<>();
+        int pl = 0;
         q.add(new Pair(root,0));
         while(q.size()!=0){
             Pair front = q.remove();
-            TreeNode left = front.node.left;
-            TreeNode right = front.node.right;
-            if(front.level!=prevlevel){
-                ans.add(List);
-                prevlevel++;
-                List = new ArrayList<Integer>();
+            TreeNode node = front.node;
+            int level = front.level;
+            if(front.level != pl){
+                ans.add(list);
+                pl++;
+                list = new ArrayList<Integer>();
             }
-            List.add(front.node.val);
-            if(left!=null) q.add(new Pair(left,front.level+1));
-            if(right!=null) q.add(new Pair(right,front.level+1));
+            list.add(front.node.val);
+            if(front.node.left!=null) q.add(new Pair(front.node.left,pl+1));
+            if(front.node.right!=null) q.add(new Pair(front.node.right,pl+1));
         }
-        ans.add(List);
-        return ans; 
+        ans.add(list);
     }
-    
 }
