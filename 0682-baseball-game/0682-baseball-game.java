be084071ec94255235802ba9b11ba4e0e1d5f8bb
aaravagrawal9084-hub <aaravagrawal9084@gmail.com>
2026-09-25 19:04:23 +0530

    Time: 3 ms (79.03%), Space: 43.8 MB (5.45%) - LeetHub

---
+++ 0682-baseball-game/0682-baseball-game.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int calPoints(String[] arr) {
+        Stack<Integer> st = new Stack<>();
+        for(int i = 0;i<arr.length;i++){
+            String s = arr[i];
+            if(s.equals("D")){
+                st.push(2*st.peek());
+            }
+            else if(s.equals("C")){
+                st.pop();
+            }
+            else if(s.equals("+")){
+               int x = st.pop();
+               int y = st.pop();
+               int z = x +y;
+               
+               st.push(y);
+               st.push(x);
+               st.push(z);
+            }
+            else{
+                st.push(Integer.parseInt(s));
+            }
+        }
+        int sum2 = 0;
+        while(st.size()>0){
+            sum2 = sum2 + st.pop();
+        }
+        return sum2;
+    }
+}
